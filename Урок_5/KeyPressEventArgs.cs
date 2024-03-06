750ffac80c745bc31314033f4d949b9331c10778
Александр Ильин <sasha8@list.ru>
2024-03-06 19:17:47 +0300

    Задание 5 выполнено

---
+++ Урок_5/KeyPressEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v02
+{
+    /// <summary>
+    /// Данный класс используется для передачи данных о нажатой клавише
+    /// </summary>
+    internal class KeyPressEventArgs:EventArgs
+    {
+        public ConsoleKeyInfo KeyInfo { get; }
+        public KeyPressEventArgs ( ConsoleKeyInfo keyInfo )
+        {
+            KeyInfo = keyInfo;
+        }
+    }
+}
---
+++ Урок_5/KeyPressEventHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_v02
+{
+    /// <summary>
+    /// Данный класс контролирует ввод и оповещает о 
+    /// введенном символе подписанные на него классы
+    /// </summary>
+    internal class KeyPressEventHandler
+    {
+        /// <summary>
+        /// событие на базе стандартного делегата EventHandler
+        /// </summary>
+        public event EventHandler<KeyPressEventArgs>? KeyPressed;
+
+        internal void WaitingForInput ( )
+        {
+            Console.WriteLine( "Вводите выражение как обычно.\n" +
+                "'ESC' - выход из программы\n" +
+                "'DELETE' - предыдущий результат\n" +
+                "'Backspace' - удаление введенного символа\n"+
+                "'=' - получение результата\n" +
+                "Поддерживаются : ' + '  ' - ' ' * ' ' / ' \n"+
+                "Только два операнда\n");
+            bool exit = false;
+            while ( !exit )
+            {
+                var keyInfo = Console.ReadKey( true );
+                exit = keyInfo.Key == ConsoleKey.Escape;
+                OnKeyPressed( new KeyPressEventArgs( keyInfo ) );
+            }
+            Console.WriteLine( "Программа завершена." );
+        }
+
+        protected virtual void OnKeyPressed ( KeyPressEventArgs e )
+        {
+            KeyPressed?.Invoke( this , e );
+        }
+
+    }
+}
